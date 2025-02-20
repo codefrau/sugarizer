ddec1d2759ffe4eba64214ce86f28332599e62cb
Vanessa Freudenberg <vanessa@codefrau.net>
2025-02-19 23:46:42 -0800

    Etoys: write to journal (WIP)

--- activities/Etoys.activity/js/activity.js
+++ activities/Etoys.activity/js/activity.js
@@ -1,7 +1,38 @@
 // Etoys activity for Sugarizer
 // using SqueakJS https://squeak.js.org
 
-requirejs(["domReady!", "squeak"], () => {
+define([
+	"sugar-web/activity/activity",
+	"sugar-web/env",
+	"squeak",
+	], (
+	activity,
+	env,
+	) => {
+	activity.setup();
+
+	env.getEnvironment((err, environment) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log("env", environment);
+
+		if (!environment.objectId) {
+			console.log("New instance");
+		} else {
+			activity.getDatastoreObject().loadAsText((error, metadata, data) => {
+				if (error === null) {
+					console.log("read done.");
+					console.log("metadata", metadata);
+					console.log("data", data);
+				} else {
+					console.log("read failed.");
+				}
+			});
+		}
+	});
+
 	SqueakJS.runSqueak(
 		'resources/etoys.image',
 		sqCanvas,
@@ -13,7 +44,32 @@ requirejs(["domReady!", "squeak"], () => {
 			files: ["etoys.changes", "EtoysV5.stc"], // same dir as etoys.image
 			root: "/Etoys",
 			templates: { "/Etoys": "." },	         // same dir too
-			onQuit: () => history.back(),
+			onQuit: () => {
+				// // Set metadata
+				// activity.getDatastoreObject().setMetadata({
+				// 	title: "Etoys Activity",
+				// 	title_set_by_user: "0",
+				// });
+
+				// Set data
+				const stateObj = { foo: "bar" };
+				activity.getDatastoreObject().setDataAsText(stateObj);
+
+				// Save the object
+				activity.getDatastoreObject().save((error) => {
+					if (error === null) {
+						console.log("write done.");
+					} else {
+						console.log("write failed.");
+					}
+					debugger
+					activity.close();
+				});
+
+				// activity.close();
+				// history.back();
+
+			}
 		}
 	);
 });
